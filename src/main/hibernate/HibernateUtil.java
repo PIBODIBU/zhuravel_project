fb90442f690f593d0e7301fef5495f8c2eeaecfd
Roman S <romeo97934@gmail.com>
2017-02-27 19:26:32 +0200

    Hibernate timeout bug

--- src/main/hibernate/HibernateUtil.java
+++ src/main/hibernate/HibernateUtil.java
@@ -3,7 +3,7 @@ package main.hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
@@ -11,7 +11,7 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            return new AnnotationConfiguration()
+            return new Configuration()
                     .configure("hibernate.cfg.xml")
                     .buildSessionFactory();
         } catch (Throwable ex) {
